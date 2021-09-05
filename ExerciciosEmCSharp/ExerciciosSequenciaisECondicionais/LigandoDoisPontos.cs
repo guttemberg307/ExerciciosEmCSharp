a07a4b68a8f21338814355d0f6bfd22ac6704341
guttemberg307 <guto2041@hotmail.com>
2021-09-05 17:47:44 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosEmCSharp/ExerciciosSequenciaisECondicionais/LigandoDoisPontos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosEmCSharp.ExerciciosSeqüênciaisECondicionais
+{
+    class LigandoDoisPontos
+    {
+        public static void Executar()
+        {
+            
+        }
+    }
+}
---
+++ ExerciciosEmCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExerciciosEmCSharp.ExerciciosSeqüênciaisECondicionais;
+using System;
+using System.Collections.Generic;
+namespace ExerciciosEmCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>()
+            {
+
+                //{"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Exercicio N° 1 - LigandoDoisPontos",LigandoDoisPontos.Executar}
+
+            });
+             central.SelecionarEExecutar();
+        } 
+        }
+}
