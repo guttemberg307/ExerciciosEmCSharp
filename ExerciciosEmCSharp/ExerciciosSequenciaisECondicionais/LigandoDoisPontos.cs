c55e5da7de88c892499141e29e57439fb31774b8
Gutemberg Nascimento da Silva <guto2041@hotmail.com>
2021-10-23 18:48:02 -0300

    Distância entre dois pontos

--- ExerciciosEmCSharp/ExerciciosSequenciaisECondicionais/LigandoDoisPontos.cs
+++ ExerciciosEmCSharp/ExerciciosSequenciaisECondicionais/LigandoDoisPontos.cs
@@ -10,7 +10,48 @@ namespace ExerciciosEmCSharp.ExerciciosSeqüênciaisECondicionais
     {
         public static void Executar()
         {
+            Console.WriteLine(" ******** Distância entre dois pontos ***** ");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            double D;
+
+
+
+            Console.WriteLine("Insira o ponto no plano P X1");
+            double x1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o ponto no plano p Y1");
+            double y1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Insira o ponto no plano P Y2");
+            double y2 = double.Parse(Console.ReadLine());
+
             
+            Console.WriteLine("Insira o ponto no plano p X2");
+            double x2 = double.Parse(Console.ReadLine());
+
+
+           D = Math.Sqrt(Math.Pow((x2 - x1), 2) + (Math.Pow((x2 - x1), 2)));
+           
+
+            Console.WriteLine($"Resultato: {D.ToString("C")}"); // valor.ToString("C") limita a casa decimal em duas exemplo 2,12
+        
+            
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
