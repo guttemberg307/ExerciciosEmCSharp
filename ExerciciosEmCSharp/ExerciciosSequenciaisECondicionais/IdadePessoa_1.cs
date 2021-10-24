a720eb67d736e6bf76e1710abf12b14d72a9089b
Gutemberg Nascimento da Silva <guto2041@hotmail.com>
2021-10-23 21:03:25 -0300

    IdadePessoa 1

---
+++ ExerciciosEmCSharp/ExerciciosSequenciaisECondicionais/IdadePessoa_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosEmCSharp.ExerciciosSequenciaisECondicionais
+{
+    class IdadePessoa_1
+    {
+        public static void Executar()
+        {
+
+            Console.WriteLine("Idade pessoa  Meses, Dias e Anos");
+
+            Console.WriteLine("Insira a sua Idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a sua Mês: ");
+            int mes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a sua Ano: ");
+            int ano = int.Parse(Console.ReadLine());
+
+
+            int resultado = (idade * 365) + (mes * 30);
+
+            Console.WriteLine($"Quantidade de dias referente a anos:{resultado}");
+
+
+        }
+
+    }
+}
--- ExerciciosEmCSharp/Program.cs
+++ ExerciciosEmCSharp/Program.cs
@@ -13,7 +13,8 @@ namespace ExerciciosEmCSharp
 
                 //{"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Exercicio N° 1 - Ligando Dois Pontos",LigandoDoisPontos.Executar},
-                {"Exercicio N° 2 - Calcular Expressão",CalcularExpressao.Executar}
+                {"Exercicio N° 2 - Calcular Expressão",CalcularExpressao.Executar},
+                {"Exercicio N° 3 - IdadePessoa 1", IdadePessoa_1.Executar }
 
             });
              central.SelecionarEExecutar();
